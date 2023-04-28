7bbbe057cc03c7def23bc1a61324dd9b4606fa44
Angeleena <angeleena@qburst.com>
2023-04-28 10:28:53 +0530

    Shell script to check pod restart/fail

--- pod_restart_fail.sh
+++ pod_restart_fail.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Namespace to be used
 NAMESPACE="default"
+count=0
 
 #Retrieve all the pods in a given namespace
 PODS=$(kubectl get pods -n $NAMESPACE -o=jsonpath='{.items[*].metadata.name}')
@@ -12,10 +13,13 @@ for pod in $PODS
         POD_STATUS=$(kubectl get pod $pod -n $NAMESPACE -o jsonpath='{.status.phase}') 
         POD_RESTART_COUNT=$(expr $(kubectl get pod $pod -n $NAMESPACE -o jsonpath='{.status.containerStatuses[*].restartCount}') + 0)
         if  [ "$POD_STATUS" = "Failed" ] || [ $POD_RESTART_COUNT -gt 0 ]; then
-            printf "Failed Pod name: $pod \n Pod status: $NAMESPACE \n Pod restart count: $NAMESPACE \n"
+            count=$((count+1))
+            printf "Failed Pod name: $pod \n Pod status: $POD_STATUS \n Pod restart count: $POD_RESTART_COUNT\n"
         fi        
     done
-printf "No Pods in failed status"
+if [ $count -eq 0 ]; then
+  printf "No Pods in failed/restart status"
+fi    
 
 
 
