3cd1cf9e26e594bb6f638b876adb6525ec44f404
abhishekarun <abhishek-qb>
2023-05-15 12:56:44 +0530

    Updated changes as per PR review

--- scripts/ec2dailysnapshots/ec2snapshot.py
+++ scripts/ec2dailysnapshots/ec2snapshot.py
@@ -3,13 +3,14 @@ import csv
 from botocore.exceptions import ProfileNotFound
 
 #Modify the following variables before running the scripts
-profile_name = ''
-Numberofsnap = 1
-snapshots=[]
 region='us-east-1'
-file_path= ''
+file_path= '/home/abhishekarun/git/DevOps-Automations/scripts/ec2dailysnapshots/instance_ids.csv'
+Numberofsnap = 2
 
 #Setting up connection and verfying the profile name
+profile_name = 'snap'
+instances=[]
+
 session = boto3.Session(profile_name=profile_name)
 ec2 = session.client('ec2', region_name=region)
 try:
@@ -17,6 +18,7 @@ try:
     response = iam_client.get_user()
 except ProfileNotFound:
     print("AWS profile '{profile_name}' does not exist")
+
  
 #Open and read the CSV file containing the Instance Id's
 with open(file_path,'r') as file:
@@ -24,9 +26,9 @@ with open(file_path,'r') as file:
     next(reader) 
     for row in reader:
         instance_id = row[0]
-        snapshots.append((instance_id, Numberofsnap))
-#Initiating snapshot for each volume available attached to the instance and puring the oldest snapshots        
-for instance, max_snapshots in snapshots:
+        instances.append((instance_id))
+#Initiating snapshot for each volume available attached to the instance and purging the oldest snapshots        
+for instance in instances:
   response = ec2.describe_volumes(Filters=[{'Name': 'attachment.instance-id', 'Values': [instance]}])
   for volume in response['Volumes']:
     volume_id = volume['VolumeId']
@@ -42,6 +44,6 @@ for instance, max_snapshots in snapshots:
     
     snaps= ec2.describe_snapshots(Filters=[{'Name': 'volume-id', 'Values': [volume_id]}])
     old_snapshots = sorted(snaps['Snapshots'], key=lambda x: x['StartTime'], reverse=True)
-    for i in range(int(max_snapshots), len(old_snapshots)):
+    for i in range(Numberofsnap, len(old_snapshots)):
           snapshot_to_delete = old_snapshots[i]['SnapshotId']
           ec2.delete_snapshot(SnapshotId=snapshot_to_delete)
