a14d6e8017192687bc8023d28f2f8554384acfed
Indrajith Sajeevan <indrajith@qburst.com>
2023-07-19 13:27:19 +0530

    Added example for cloud function v2

---
+++ terraform/gcp/examples/cloud_function_v2/main.tf
@@ -0,0 +1,15 @@
+module "cloud_function_v2" {
+  source = "/.../.../gcp/modules/cloud_function_v2"
+  function_name = "new-function"
+  location = "us-central1"
+  make_public = true
+  config = {
+    runtime = "python310"
+    entry_point = "hello_http"
+  }
+
+}
+
+output "cloud_function_v2_module_outputs" {
+  value = module.cloud_function_v2
+}
