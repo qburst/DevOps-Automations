d211b1c443a836ce0f0fcb93aec42a77146a9a8a
Ummu Salma <ummu.salma@qburst.com>
2023-09-15 13:17:17 +0530

    added example

--- terraform/aws/examples/elasticache/main.tf
+++ terraform/aws/examples/elasticache/main.tf
@@ -1,16 +1,16 @@
 module "elasticache" {
-	source               	  = "../../modules/elasticache"
-	name	             	  = "qburst"
-	environment          	  = "test"
-	vpc_id               	  = "vpc-0aaada0de503478d5"
-	allowed_cidr        	  = ["10.51.0.0/16"]
-	subnet_ids          	  = ["subnet-043f47df947e369ca", "subnet-055a33bbac0724bb4", "subnet-0bff946a98d3aac91"]
-	parameter_group_name 	  = "memcached-pg"
-	maintenance_window     	  = "sat:07:30-sat:08:30"
-	engine				 	  = "memcached"
-	replication_group_enabled = "enabled"
-	port			    	  = 11211
-	parameter_group_family    = "memcached1.6"
+	source				  = "../../modules/elasticache"
+	Name				  = "qburst"
+	environment			  = "test"
+	vpc_id				  = "vpc-0aaada0de503478d5"
+	allowed_cidr     		  = ["10.51.0.0/16"]
+	subnet_ids			  = ["subnet-043f47df947e369ca", "subnet-055a33bbac0724bb4", "subnet-0bff946a98d3aac91"]
+	parameter_group_name              = "memcached-pg"
+	maintenance_window		  = "sat:07:30-sat:08:30"
+	engine				  = "memcached"
+	replication_group_enabled 	  = "enabled"
+	port				  = 11211
+	parameter_group_family		  = "memcached1.6"
 }
 
 output "example_outputs" {
