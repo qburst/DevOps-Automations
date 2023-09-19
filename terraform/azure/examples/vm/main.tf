9d5895272cdfc5569f14a1bc1f3cfffed7f941b2
John William <sjohn51095@gmail.com>
2023-09-19 14:41:25 +0530

    Formatted styles and lines

--- terraform/azure/examples/vm/main.tf
+++ terraform/azure/examples/vm/main.tf
@@ -1,11 +1,11 @@
 module "azure_vm" {
-  source               = "../../modules/azure-vm"
-  vm_name              = var.vm_name
-  location             = var.location
-  resource_group_name  = var.resource_group_name
-  subnet_id            = module.azurerm_subnet.subnet.id
-  vm_size              = var.vm_size
-  admin_username       = var.admin_username
-  admin_password       = var.admin_password
-  tags                 = var.tags
+  source              = "../../modules/azure-vm"
+  vm_name             = var.vm_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = module.azurerm_subnet.subnet.id
+  vm_size             = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  tags                = var.tags
 }
--- terraform/azure/examples/vm/terraform.tfvars
+++ terraform/azure/examples/vm/terraform.tfvars
@@ -1,14 +1,14 @@
 
-vm_name          = "my-vm"
-location         = "East US"
+vm_name             = "my-vm"
+location            = "East US"
 resource_group_name = "rg-sample"
-vm_size          = "Standard_DS1_v2"
-publisher = "Canonical"
-offer = "UbuntuServer"
-sku="20.04 LTS"
-version="latest"
-admin_username   = "adminuser"
-admin_password   = "P@ssw0rd123"
+vm_size             = "Standard_DS1_v2"
+publisher           = "Canonical"
+offer               = "UbuntuServer"
+sku                 = "20.04 LTS"
+version             = "latest"
+admin_username      = "adminuser"
+admin_password      = "P@ssw0rd123"
 tags = {
   environment = "development"
 }
