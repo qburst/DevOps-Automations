6845676cb178093a3642c865c7b6deff23b2356e
gopinath.k <gopinath.k@qburst.com>
2023-09-22 17:59:10 +0530

    add options eip, sg, disk, subnet, root disk parameter

--- terraform/aws/modules/ec2/main.tf
+++ terraform/aws/modules/ec2/main.tf
@@ -1,12 +1,93 @@
 provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region = "ap-south-1"
+  profile = "default"
+  region  = "ap-south-1"
 }
 
-resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    count = "${var.number_of_instances}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.ami_key_pair_name}"
+# create Security Group
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_default_security_group" "default" {
+vpc_id = data.aws_vpc.default.id
+}
+resource "aws_security_group" "sec_group" {
+  name = "default"
+}
+
+resource "aws_security_group" "aws-sg" {
+  name        = "teraform-sg"
+  description = "Allow ingress traffic via Terraform"
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      description = lookup(ingress.value, "description", null)
+      from_port   = lookup(ingress.value, "from_port", null)
+      to_port     = lookup(ingress.value, "to_port", null)
+      protocol    = lookup(ingress.value, "protocol", null)
+      cidr_blocks = lookup(ingress.value, "cidr_blocks", null)
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      description = lookup(egress.value, "description", null)
+      from_port   = lookup(egress.value, "from_port", null)
+      to_port     = lookup(egress.value, "to_port", null)
+      protocol    = lookup(egress.value, "protocol", null)
+      cidr_blocks = lookup(egress.value, "cidr_blocks", null)
+    }
+  }
+
+  tags = {
+    Name = "teraform-sg"
+  }
+
+}
+
+# Create ec2 instance
+resource "aws_instance" "instance" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.ami_key_pair_name != "" ? var.ami_key_pair_name : ""
+  iam_instance_profile        = var.iam-role-name != "" ? var.iam-role-name : ""
+  associate_public_ip_address = var.instance-associate-public-ip
+  vpc_security_group_ids      = [aws_default_security_group.default.id, aws_security_group.aws-sg.id]
+  subnet_id                   = var.subnet-id != "" ? var.subnet-id : ""
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = "8"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "${var.instance-tag-name}"
+  }
+}
+
+# Attach EIP
+resource "aws_eip_association" "eip-association" {
+  count         = var.alloc_id == null ? 0 : 1
+  instance_id   = aws_instance.instance.id
+  allocation_id = var.alloc_id
+}
+
+# Create EBS volume
+resource "aws_ebs_volume" "ebs_volume" {
+  count             = var.ec2_additional_ebs_volume_count
+  availability_zone = aws_instance.instance.availability_zone
+  size              = var.ec2_additional_ebs_volume_size
+  tags = {
+    Name = "${var.disk-tag-name}_${count.index + 1}"
+  }
+}
+
+# Attach EBS Volume
+resource "aws_volume_attachment" "volume_attachment" {
+  count       = var.ec2_additional_ebs_volume_count
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ebs_volume[0].id
+  instance_id = aws_instance.instance.id
 }
--- terraform/aws/modules/ec2/variables.tf
+++ terraform/aws/modules/ec2/variables.tf
@@ -1,31 +1,103 @@
-variable "access_key" {
-        description = "Access key to AWS console"
-}
-variable "secret_key" {
-        description = "Secret key to AWS console"
-}
-
-
-variable "instance_name" {
-        description = "Name of the instance to be created"
-        default = "awsbuilder-demo"
-}
-
+# EC2
 variable "instance_type" {
-        default = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "ami_id" {
-        description = "The AMI to use"
-        default = "ami-08e5424edfe926b43"
+  description = "The AMI to use"
+  default     = "ami-08e5424edfe926b43"
 }
 
 variable "number_of_instances" {
-        description = "number of instances to be created"
-        default = 1
+  description = "number of instances to be created"
+  default     = 1
 }
 
 
 variable "ami_key_pair_name" {
-        default = "aws-devops"
+  default = "ec2-terraform"
+}
+
+variable "instance-tag-name" {
+  description = "instance-tag-name"
+  type        = string
+  default     = "EC2-instance-created-with-terraform"
+}
+
+variable "instance-associate-public-ip" {
+  description = "Defines if the EC2 instance has a public IP address."
+  type        = string
+  default     = "true"
+}
+
+# IAM
+variable "iam-role-name" {
+  default = ""
+}
+
+# EIP
+variable "alloc_id" {
+  default = ""
+}
+
+# SG
+variable "ingress_rules" {
+  default = {
+    "http ingress rule" = {
+      "description" = "For HTTP"
+      "from_port"   = "80"
+      "to_port"     = "80"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    },
+    "ssh ingress rule" = {
+      "description" = "For SSH"
+      "from_port"   = "22"
+      "to_port"     = "22"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    }
+  }
+  type        = map(any)
+  description = "Security group rules"
+}
+
+variable "egress_rules" {
+  default = {
+    "http ingress rule" = {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  } }
+  type        = map(any)
+  description = "Security group rules"
+}
+
+# SUBNET
+variable "subnet-id" {
+  default = ""
+}
+
+# DISK
+variable "root_volume_type" {
+  default = "gp2"
+}
+
+variable "root_volume_size" {
+  default = "8"
+} 
+
+variable "disk-tag-name" {
+  description = "terraform vol"
+  type        = string
+  default     = "terraform-vol"
+}
+
+variable "ec2_additional_ebs_volume_count" {
+  default = 2
+}
+
+variable "ec2_additional_ebs_volume_size" {
+  default = 2
 }
