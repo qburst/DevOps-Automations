4ba4a17960fccadeb6278aeb2de134dff3a3b332
Angeleena <angeleena@qburst.com>
2023-09-22 16:48:22 +0530

    Stable image version of Ubuntu 22.04

--- terraform/gcp/examples/compute_engine/main.tf
+++ terraform/gcp/examples/compute_engine/main.tf
@@ -2,7 +2,7 @@ module "compute_engine" {
   source       = "/../../gcp/modules/compute_engine"
   name         = "Qburst-DevOps"
   machine_type = "e2-medium"
-  image        = "ubuntu-os-cloud/ubuntu-2004-lts"
+  image        = "ubuntu-os-cloud/ubuntu-2204-lts"
   zone         = "us-central1-a"
   network      = "10.1.1.0"
 
--- terraform/gcp/modules/compute_engine/variables.tf
+++ terraform/gcp/modules/compute_engine/variables.tf
@@ -13,7 +13,7 @@ variable "zone" {
 }
 variable "image" {
   type = string
-  default = "ubuntu-os-cloud/ubuntu-2004-lts"
+  default = "ubuntu-os-cloud/ubuntu-2204-lts"
 }
 variable "network" {
   type = string
