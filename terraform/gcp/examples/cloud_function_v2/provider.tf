c6ff03bb97aaef3fa4c9884ac06339646a7c049a
Praven John <praven@qburst.com>
2023-09-26 13:22:31 +0530

    Updating the TF formatting

--- terraform/gcp/examples/cloud_function_v2/provider.tf
+++ terraform/gcp/examples/cloud_function_v2/provider.tf
@@ -1,5 +1,5 @@
-provider "google"{
-    project = "qburst-devops-automation"
-    region = "us-central1"
-    credentials = "${file("credentials.json")}"
+provider "google" {
+  project     = "qburst-devops-automation"
+  region      = "us-central1"
+  credentials = file("credentials.json")
 }
