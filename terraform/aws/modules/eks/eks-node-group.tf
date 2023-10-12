4601204f0324b7657ffc16daa965a34a6ecb1579
Rithin-QB <rithin.das@qburst.com>
2023-10-12 17:26:04 +0530

    Added Variables for eks-node-group configuration

--- terraform/aws/modules/eks/eks-node-group.tf
+++ terraform/aws/modules/eks/eks-node-group.tf
@@ -14,48 +14,47 @@ resource "aws_iam_role" "nodes_general" {
   ]
 }
 POLICY
-
 }
+
 resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy_general" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role = aws_iam_role.nodes_general.name
+  role       = aws_iam_role.nodes_general.name
 }
+
 resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy_general" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role = aws_iam_role.nodes_general.name
+  role       = aws_iam_role.nodes_general.name
 }
+
 resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role = aws_iam_role.nodes_general.name
+  role       = aws_iam_role.nodes_general.name
 }
 
 resource "aws_eks_node_group" "nodes_general" {
-  cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "nodes-general"
+  cluster_name    = var.eks_cluster_name
+  node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.nodes_general.arn
-  subnet_ids      = [
-    aws_subnet.private_us_east_1a.id,
-    aws_subnet.private_us_east_1b.id
-  ]
-scaling_config {
-  desired_size = 1
-  max_size = 1
-  min_size = 1
+  subnet_ids      = var.node_group_subnet_ids
 
-}
-ami_type = "AL2_x86_64"
-capacity_type = "ON_DEMAND"
-disk_size = 20
-force_update_version = false
-instance_types = ["t3.small"]
-labels = {
-    role = "nodes_general"
-}
-version = "1.23"
+  scaling_config {
+    desired_size = var.node_group_desired_size
+    max_size     = var.node_group_max_size
+    min_size     = var.node_group_min_size
+  }
 
-depends_on = [ 
+  ami_type        = var.node_group_ami_type
+  capacity_type   = var.node_group_capacity_type
+  disk_size       = var.node_group_disk_size
+  force_update_version = false
+  instance_types  = var.node_group_instance_types
+  labels          = var.node_group_labels
+  version         = var.node_group_version
+
+  depends_on = [
     aws_iam_role_policy_attachment.amazon_eks_worker_node_policy_general,
     aws_iam_role_policy_attachment.amazon_eks_cni_policy_general,
     aws_iam_role_policy_attachment.amazon_ec2_container_registry_read_only_policy
- ]
+  ]
 }
+
--- terraform/aws/modules/eks/variables.tf
+++ terraform/aws/modules/eks/variables.tf
@@ -29,12 +29,94 @@ variable "nat_gateway_name" {
 }
 
 variable "eks_cluster_name" {
-  description = "Name for the EKS Cluster"
   type        = string
-  default     = "demo"
+  description = "Name of the EKS cluster."
+  default     = "my-eks-cluster"
 }
-variable "eks_node_group_name" {
-  description = "Name of the EKS node group"
+
+variable "eks_cluster_version" {
   type        = string
+  description = "Version of the EKS cluster."
+  default     = "1.24"
+}
+
+variable "eks_cluster_region" {
+  type        = string
+  description = "AWS region for the EKS cluster."
+  default     = "us-east-1"
+}
+
+variable "node_group_name" {
+  type        = string
+  description = "Name of the EKS node group."
   default     = "nodes-general"
 }
+
+variable "node_group_subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs for the node group."
+  default     = ["subnet-06296791b48518207",
+                "subnet-09b57ef336acf2868",
+                "subnet-0bf698a94602fa750",
+                "subnet-086708cfd5a439019"
+]
+}
+
+variable "node_group_desired_size" {
+  type        = number
+  description = "Desired size of the node group."
+  default     = 1
+}
+
+variable "node_group_max_size" {
+  type        = number
+  description = "Maximum size of the node group."
+  default     = 2
+}
+
+variable "node_group_min_size" {
+  type        = number
+  description = "Minimum size of the node group."
+  default     = 1
+}
+
+variable "node_group_ami_type" {
+  type        = string
+  description = "AMI type for the node group (e.g., AL2_x86_64)."
+  default     = "AL2_x86_64"
+}
+
+variable "node_group_capacity_type" {
+  type        = string
+  description = "Capacity type for the node group (e.g., ON_DEMAND)."
+  default     = "ON_DEMAND"
+}
+
+variable "node_group_disk_size" {
+  type        = number
+  description = "Disk size (in GB) for nodes in the group."
+  default     = 20
+}
+
+variable "node_group_instance_types" {
+  type        = list(string)
+  description = "List of instance types for the node group."
+  default     = ["t3.small"]
+}
+
+variable "node_group_labels" {
+  type        = map(string)
+  description = "Labels for the node group instances."
+  default     = { role = "nodes_general" }
+}
+
+variable "node_group_version" {
+  type        = string
+  description = "Version for the node group."
+  default     = "1.24"
+}
+
+# Define other variables as needed.
+
+
+
