f4fa5b8adf21f1543567e2055968e1c5675f3c5d
SulthanaSaleem-QB <143723244+SulthanaSaleem-QB@users.noreply.github.com>
2024-07-23 16:53:28 +0530

    Updated the mysqldb module

--- terraform/azure/modules/mysql-db/main.tf
+++ terraform/azure/modules/mysql-db/main.tf
@@ -4,54 +4,41 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "azrg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.azrg.name
-  address_space       = var.address_space
-}
-
-resource "azurerm_subnet" "snet" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.azrg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.subnet_address_prefixes
-  service_endpoints    = var.subnet_service_endpoints
-
-  delegation {
-    name = var.subnet_delegation_name
-    service_delegation {
-      name    = var.subnet_service_delegation_name
-      actions = var.subnet_service_delegation_actions
-    }
-  }
+# Module for network
+module "network" {
+  source                  = "../vnet"
+  vnet_name               = var.vnet_name
+  address_space           = var.vnet_address_space
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  subnet_names            = var.subnet_names
+  subnet_address_prefixes = var.subnet_address_prefixes
+  nsg_name                = var.nsg_name
+  inbound_rules           = var.inbound_rules
+  outbound_rules          = var.outbound_rules
+  environment             = var.environment
 }
 
 resource "azurerm_private_dns_zone" "domain" {
   name                = var.private_dns_zone_name
-  resource_group_name = azurerm_resource_group.azrg.name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vlink" {
   name                  = var.private_dns_zone_link_name
   private_dns_zone_name = azurerm_private_dns_zone.domain.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-  resource_group_name   = azurerm_resource_group.azrg.name
+  virtual_network_id    = module.network.vnet_id
+  resource_group_name   = var.resource_group_name
 }
 
 resource "azurerm_mysql_flexible_server" "sqlserver" {
   name                   = var.mysql_server_name
-  resource_group_name    = azurerm_resource_group.azrg.name
-  location               = azurerm_resource_group.azrg.location
+  resource_group_name    = var.resource_group_name
+  location               = var.location
   administrator_login    = var.mysql_admin_login
   administrator_password = var.mysql_admin_password
   backup_retention_days  = var.mysql_backup_retention_days
-  delegated_subnet_id    = azurerm_subnet.snet.id
+  delegated_subnet_id    = module.network.subnet_ids[0] # Using the first subnet ID
   private_dns_zone_id    = azurerm_private_dns_zone.domain.id
   sku_name               = var.mysql_sku_name
   zone                   = var.mysql_zone
@@ -61,7 +48,7 @@ resource "azurerm_mysql_flexible_server" "sqlserver" {
 
 resource "azurerm_mysql_flexible_database" "sqldb" {
   name                = var.mysql_database_name
-  resource_group_name = azurerm_resource_group.azrg.name
+  resource_group_name = var.resource_group_name
   server_name         = azurerm_mysql_flexible_server.sqlserver.name
   charset             = var.mysql_database_charset
   collation           = var.mysql_database_collation
@@ -69,8 +56,23 @@ resource "azurerm_mysql_flexible_database" "sqldb" {
 
 resource "azurerm_mysql_flexible_server_firewall_rule" "firewall" {
   name                = var.mysql_firewall_rule_name
-  resource_group_name = azurerm_resource_group.azrg.name
+  resource_group_name = var.resource_group_name
   server_name         = azurerm_mysql_flexible_server.sqlserver.name
   start_ip_address    = var.mysql_firewall_start_ip
   end_ip_address      = var.mysql_firewall_end_ip
+}
+
+# Private Endpoint
+resource "azurerm_private_endpoint" "mysql_private_endpoint" {
+  name                = var.mysql_private_endpoint_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = module.network.subnet_ids[0] # Using the first subnet ID
+
+  private_service_connection {
+    name                           = var.mysql_private_endpoint_connection_name
+    private_connection_resource_id = azurerm_mysql_flexible_server.sqlserver.id
+    subresource_names              = ["mysqlserver"]
+    is_manual_connection           = false
+  }
 }
